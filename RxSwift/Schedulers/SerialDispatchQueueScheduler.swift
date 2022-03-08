74890228c96779b7dab3a583e7fc74d8e5d61baf
JustinLau <janstilau@gmail.com>
2022-03-08 11:56:58 +0800

    sync

--- RxSwift/Schedulers/SerialDispatchQueueScheduler.swift
+++ RxSwift/Schedulers/SerialDispatchQueueScheduler.swift
@@ -26,6 +26,7 @@ In case some customization need to be made on it before usage,
 internal serial queue can be customized using `serialQueueConfiguration`
 callback.
 */
+
 public class SerialDispatchQueueScheduler : SchedulerType {
     
     public typealias TimeInterval = Foundation.TimeInterval
@@ -38,16 +39,12 @@ public class SerialDispatchQueueScheduler : SchedulerType {
 
     let configuration: DispatchQueueConfiguration
     
-    /**
-    Constructs new `SerialDispatchQueueScheduler` that wraps `serialQueue`.
-
-    - parameter serialQueue: Target dispatch queue.
-    - parameter leeway: The amount of time, in nanoseconds, that the system will defer the timer.
-    */
+    // 如果, 直接传递过来一个 DispatchQueue, 就直接使用过了.
     init(serialQueue: DispatchQueue, leeway: DispatchTimeInterval = DispatchTimeInterval.nanoseconds(0)) {
         self.configuration = DispatchQueueConfiguration(queue: serialQueue, leeway: leeway)
     }
 
+    // 内部创建一个串行队列.
     public convenience init(internalSerialQueueName: String, serialQueueConfiguration: ((DispatchQueue) -> Void)? = nil, leeway: DispatchTimeInterval = DispatchTimeInterval.nanoseconds(0)) {
         // 内部创建一个 DispatchQueue
         let queue = DispatchQueue(label: internalSerialQueueName, attributes: [])
@@ -55,13 +52,7 @@ public class SerialDispatchQueueScheduler : SchedulerType {
         self.init(serialQueue: queue, leeway: leeway)
     }
     
-    /**
-    Constructs new `SerialDispatchQueueScheduler` named `internalSerialQueueName` that wraps `queue`.
-    
-    - parameter queue: Possibly concurrent dispatch queue used to perform work.
-    - parameter internalSerialQueueName: Name of internal serial dispatch queue proxy.
-    - parameter leeway: The amount of time, in nanoseconds, that the system will defer the timer.
-    */
+    // 内部创建一个串行队列.
     public convenience init(queue: DispatchQueue, internalSerialQueueName: String, leeway: DispatchTimeInterval = DispatchTimeInterval.nanoseconds(0)) {
         // Swift 3.0 IUO
         let serialQueue = DispatchQueue(label: internalSerialQueueName,
@@ -70,25 +61,6 @@ public class SerialDispatchQueueScheduler : SchedulerType {
         self.init(serialQueue: serialQueue, leeway: leeway)
     }
 
-    /**
-     Constructs new `SerialDispatchQueueScheduler` that wraps one of the global concurrent dispatch queues.
-     
-     - parameter qos: Identifier for global dispatch queue with specified quality of service class.
-     - parameter internalSerialQueueName: Custom name for internal serial dispatch queue proxy.
-     - parameter leeway: The amount of time, in nanoseconds, that the system will defer the timer.
-     */
-    @available(macOS 10.10, *)
-    public convenience init(qos: DispatchQoS, internalSerialQueueName: String = "rx.global_dispatch_queue.serial", leeway: DispatchTimeInterval = DispatchTimeInterval.nanoseconds(0)) {
-        self.init(queue: DispatchQueue.global(qos: qos.qosClass), internalSerialQueueName: internalSerialQueueName, leeway: leeway)
-    }
-    
-    /**
-    Schedules an action to be executed immediately.
-    
-    - parameter state: State passed to the action to be executed.
-    - parameter action: Action to be executed.
-    - returns: The disposable object used to cancel the scheduled action (best effort).
-    */
     public final func schedule<StateType>(_ state: StateType, action: @escaping (StateType) -> Disposable) -> Disposable {
         self.scheduleInternal(state, action: action)
     }
@@ -97,27 +69,10 @@ public class SerialDispatchQueueScheduler : SchedulerType {
         self.configuration.schedule(state, action: action)
     }
 
-    /**
-    Schedules an action to be executed.
-    
-    - parameter state: State passed to the action to be executed.
-    - parameter dueTime: Relative time after which to execute the action.
-    - parameter action: Action to be executed.
-    - returns: The disposable object used to cancel the scheduled action (best effort).
-    */
     public final func scheduleRelative<StateType>(_ state: StateType, dueTime: RxTimeInterval, action: @escaping (StateType) -> Disposable) -> Disposable {
         self.configuration.scheduleRelative(state, dueTime: dueTime, action: action)
     }
     
-    /**
-    Schedules a periodic piece of work.
-    
-    - parameter state: State passed to the action to be executed.
-    - parameter startAfter: Period after which initial work should be run.
-    - parameter period: Period for running the work periodically.
-    - parameter action: Action to be executed.
-    - returns: The disposable object used to cancel the scheduled action (best effort).
-    */
     public func schedulePeriodic<StateType>(_ state: StateType, startAfter: RxTimeInterval, period: RxTimeInterval, action: @escaping (StateType) -> StateType) -> Disposable {
         // 直接, 使用了 DispatchQueueConfiguration 完成了定时器的工作. 
         self.configuration.schedulePeriodic(state, startAfter: startAfter, period: period, action: action)
